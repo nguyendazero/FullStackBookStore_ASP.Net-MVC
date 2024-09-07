b80243c4624907487796ed4ed3472b91847050fa
nguyendazero <nguyendazero@gmail.com>
2024-09-07 14:11:25 +0700

    Completed all IActionResult

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNet.BookStore.Models;
 using DotNet.BookStore.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,6 +23,26 @@ namespace DotNet.BookStore.Controllers
         public IActionResult Index()
         {
 
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Update(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Save(Book book)
+        {
             return View();
         }
     }
--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNet.BookStore.Models;
 using DotNet.BookStore.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,6 +25,26 @@ namespace DotNet.BookStore.Controllers
         public IActionResult Index()
         {
 
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Update(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Save(Order order)
+        {
             return View();
         }
     }
